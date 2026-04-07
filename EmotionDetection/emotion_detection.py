e486033f3ce8e09cebf465142a202a6b0aa92991
Xavier Breton <xbreton@gmail.com>
2026-04-07 00:02:42 -0400

    Fixed errors

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def emotion_predictor(text_to_analyse):
+def emotion_detector(text_to_analyse):
     # API configuration
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .emotion_detection import emotion_predictor
